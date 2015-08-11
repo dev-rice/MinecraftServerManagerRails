f03cc268e8f09b3c4f9c4942b46d3e5e59cd2ea2
Chris Rice <chrisrice93@gmail.com>
2015-08-10 21:33:34 -0600

    Linked up postgres to manage a server instance across page reloads. Added buttons for starting and stopping the server.

--- app/controllers/minecraft_server_controller.rb
+++ app/controllers/minecraft_server_controller.rb
@@ -19,39 +19,105 @@ class MinecraftWorld
 end
 
 class MinecraftServer
-    attr_reader :pid, :running, :pid_filepath
+    attr_reader :pid, :running, :dbconn, :table_id
+
+    def initialize(dbconn)
+        @dbconn = dbconn
+        @running = false
+        @table_id = 'server'
+        if !server_table_exists?
+            create_server_table
+        end
+        restore_if_running
+    end
+
+    def restore_if_running
+        num_rows = get_number_of_rows_in_table(table_id)
+        if (num_rows != 0)
+            restore
+        end
+    end
+
+    def restore
+        set_pid(get_pid_from_table)
+    end
+
+    def get_pid_from_table
+        result = dbconn.exec("SELECT pid FROM #{table_id}")
+        entry = result[0]
+
+        entry["pid"].to_i
+    end
+
+    def get_number_of_rows_in_table(table_name)
+        result = dbconn.exec("SELECT * FROM #{table_name};")
+
+        result.ntuples
+    end
+
+    def server_table_exists?
+        table_exists?(table_id)
+    end
+
+    def table_exists?(table_name)
+        result = dbconn.exec("SELECT count(*) FROM information_schema.tables WHERE table_name = \'#{table_name}\';")
+        count = result[0]["count"]
+
+        count != 0
+    end
+
+    def create_server_table
+        dbconn.exec("CREATE TABLE #{table_id}(pid int, running bool);")
+    end
 
     def start_world(world)
         world.start(self)
     end
 
-    def start(version)
-        filename = get_server_filename(version)
-        run_sever_command(filename)
+    def create_db_server_entry
+        dbconn.exec("INSERT INTO #{table_id} (pid, running) VALUES (#{pid}, true);")
+    end
 
+    def delete_db_server_entry
+        dbconn.exec("DELETE FROM #{table_id} WHERE pid = #{pid};")
+    end
+
+    def start(version)
+        if !running
+            filename = get_server_filename(version)
+            run_sever_command(filename)
+            create_db_server_entry
+        end
     end
 
     def run_sever_command(filename)
-        @pid = fork do
+        pid_temp = fork do
           # this code is run in the child process
           # you can do anything here, like changing current directory or reopening STDOUT
           exec "java -Xmx1024M -Xms1024M -jar #{filename} nogui"
         end
+        set_pid(pid_temp)
+    end
 
-        print_pid
-
+    def set_pid(pid_temp)
+        @pid = pid_temp
         @running = true
+    end
+
+    def kill_server
+        Process.kill(9, pid)
+        @running = false
     end
 
     def print_pid
         puts "PID: #{pid}"
     end
-    
+
     def stop
         if running
-            Process.kill(9, pid)
+            delete_db_server_entry
+            kill_server
         end
-        @running = false
     end
 
     def get_server_filename(version)
@@ -59,10 +125,20 @@ class MinecraftServer
     end
 end
 
+
+require 'pg'
+
 class MinecraftServerController < ApplicationController
-    def index
-        puts "Hello, world!"
-        start_server
+    attr_reader :server, :conn
+
+    def initialize
+        initialize_db_connection
+        @server = MinecraftServer.new(conn)
+    end
+
+    def initialize_db_connection
+        @conn = PG::Connection.open(:dbname => 'MinecraftServerManagerRails_development')
+        puts @conn.error_message
     end
 
     def start_server
@@ -70,14 +146,18 @@ class MinecraftServerController < ApplicationController
             name: "test world",
             directory: "/Users/chrisrice/MinecraftWorlds/testworld",
             version: "1.8.8")
+        if server
+            server.start_world(test_world)
+        else
+            puts "server does not exist"
+        end
+    end
 
-        server = MinecraftServer.new
-        server.start_world(test_world)
-
-        # puts "The server will be up for 5 seconds"
-        # sleep(5)
-        # puts "Im going to stop in 5 seconds"
-        # sleep(5)
-        # server.stop
+    def stop_server
+        if server
+            server.stop
+        else
+            puts "server does not exist"
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do    
+Rails.application.routes.draw do
     get 'minecraft_server/index'
     root 'minecraft_server#index'
-
+    get 'ajax/start_server' => 'minecraft_server#start_server'
+    get 'ajax/stop_server' => 'minecraft_server#stop_server'
 end
