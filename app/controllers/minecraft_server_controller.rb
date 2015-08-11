0a9da19705bcac84f03df04a9c5cfb82ae733a74
Chris Rice <chrisrice93@gmail.com>
2015-08-11 08:20:04 -0600

    Moved all database interaction code into the ServerTable class.

--- app/controllers/minecraft_server_controller.rb
+++ app/controllers/minecraft_server_controller.rb
@@ -18,45 +18,50 @@ class MinecraftWorld
 
 end
 
-class MinecraftServer
-    attr_reader :pid, :running, :dbconn, :table_id
+class ServerTable
+    attr_reader :dbconn, :table_id
 
-    def initialize(dbconn)
+    def initialize(dbconn, table_id)
         @dbconn = dbconn
-        @running = false
-        @table_id = 'server'
-        if !server_table_exists?
-            create_server_table
-        end
-        restore_if_running
-    end
+        @table_id = table_id
 
-    def restore_if_running
-        num_rows = get_number_of_rows_in_table(table_id)
-        if (num_rows != 0)
-            restore
+        if !this_table_exists?
+            create
         end
     end
 
-    def restore
-        set_pid(get_pid_from_table)
-    end
-
-    def get_pid_from_table
+    def get_pid
         result = dbconn.exec("SELECT pid FROM #{table_id}")
         entry = result[0]
 
         entry["pid"].to_i
     end
 
+    def num_rows
+        get_number_of_rows_in_table(table_id)
+    end
+
+    def this_table_exists?
+        table_exists?(table_id)
+    end
+
+    def create
+        dbconn.exec("CREATE TABLE #{table_id}(pid int, running bool);")
+    end
+
+    def create_entry(pid)
+        dbconn.exec("INSERT INTO #{table_id} (pid, running) VALUES (#{pid}, true);")
+    end
+
+    def delete_entry
+        pid = get_pid
+        dbconn.exec("DELETE FROM #{table_id} WHERE pid = #{pid};")
+    end
+
     def get_number_of_rows_in_table(table_name)
         result = dbconn.exec("SELECT * FROM #{table_name};")
 
         result.ntuples
-    end
-
-    def server_table_exists?
-        table_exists?(table_id)
     end
 
     def table_exists?(table_name)
@@ -66,34 +71,43 @@ class MinecraftServer
         count != 0
     end
 
-    def create_server_table
-        dbconn.exec("CREATE TABLE #{table_id}(pid int, running bool);")
+end
+
+class MinecraftServer
+    attr_reader :pid, :running, :server_table
+
+    def initialize(server_table)
+        @server_table = server_table
+        @running = false
+
+        restore_if_running
+    end
+
+    def restore_if_running
+        num_rows = server_table.num_rows
+        if (num_rows != 0)
+            restore
+        end
+    end
+
+    def restore
+        set_pid(server_table.get_pid)
     end
 
     def start_world(world)
         world.start(self)
     end
 
-    def create_db_server_entry
-        dbconn.exec("INSERT INTO #{table_id} (pid, running) VALUES (#{pid}, true);")
-    end
-
-    def delete_db_server_entry
-        dbconn.exec("DELETE FROM #{table_id} WHERE pid = #{pid};")
-    end
-
     def start(version)
         if !running
             filename = get_server_filename(version)
             run_sever_command(filename)
-            create_db_server_entry
+            server_table.create_entry(pid)
         end
     end
 
     def run_sever_command(filename)
         pid_temp = fork do
-          # this code is run in the child process
-          # you can do anything here, like changing current directory or reopening STDOUT
           exec "java -Xmx1024M -Xms1024M -jar #{filename} nogui"
         end
         set_pid(pid_temp)
@@ -115,7 +129,7 @@ class MinecraftServer
 
     def stop
         if running
-            delete_db_server_entry
+            server_table.delete_entry
             kill_server
         end
     end
@@ -129,11 +143,12 @@ end
 require 'pg'
 
 class MinecraftServerController < ApplicationController
-    attr_reader :server, :conn
+    attr_reader :server, :conn, :server_table
 
     def initialize
         initialize_db_connection
-        @server = MinecraftServer.new(conn)
+        @server_table = ServerTable.new(conn, 'server')
+        @server = MinecraftServer.new(server_table)
     end
 
     def initialize_db_connection
